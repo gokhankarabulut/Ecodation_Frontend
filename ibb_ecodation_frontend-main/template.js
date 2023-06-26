72e4a5c1dab552c533a93aa20ca4c7f35d181728
Gökhan Karabulut <gokhan karabulut>
2023-06-26 06:35:21 +0300

    navbar - footer kısımları düzeltildi

--- ibb_ecodation_frontend-main/template.js
+++ ibb_ecodation_frontend-main/template.js
@@ -1,1 +1,26 @@
-// alert("Deneme")
+// Dark Mode
+let darkMode =()=>{
+    document.body.classList.toggle("dark_mode")
+}
+
+// input search
+$(document).ready(function(){
+    const searchApi=["Adana","Balikesir","Çorum","Denizli","Diyarbakır","Edirne","Malatya","Van"];
+    $("#tags").autocomplete({
+        source:searchApi
+    })
+    }); 
+    //end 
+
+    //     Footer 
+    // JS DOM
+let newDate = ()=>{
+    const date = new Date().getFullYear();
+// document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+    // Jquery DOM
+    $("#date_id").html(date)
+
+
+}
+newDate();
